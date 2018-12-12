74971684070caaccb1dacacd8f7e834064f4ef7a
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2018-12-12 01:00:23 +0100

    working with pyton classes

--- hungrypy/templates.py
+++ hungrypy/templates.py
@@ -1,5 +1,24 @@
 import os
 
 
-
 # getting the template file path
+def get_template_path(path):
+    file_path = os.path.join(os.getcwd(), path)
+    if not os.path.isfile(file_path):
+        raise Exception("This is not a valid template path %s" % (file_path))
+    else:
+        print("The path:")
+    return file_path
+
+
+def get_template(path):
+    file_path = get_template_path(path)
+    return open(file_path).read()
+
+
+def render_context(template_string, context):
+    return template_string.format(**context)
+
+
+file_ = 'hungrypy/templates/email_message'
+print(get_template_path(file_))
